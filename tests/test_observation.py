a86adb4510b280b5c89b71255c50612fca7c1c40
jonasbarth <jonas.barth.95@gmail.com>
2024-04-14 15:35:21 +0200

    make extract grids compatible with batch dims

--- tests/test_observation.py
+++ tests/test_observation.py
@@ -1,6 +1,6 @@
 import torch
 
-from observation import Observation, get_visible_agent_observations
+from observation import Observation, get_visible_agent_observations, extract_observation_grids
 
 
 def test_observation():
@@ -54,3 +54,45 @@ def test_get_visible_agents_with_observation_objects():
     visible_agents = get_visible_agent_observations(coords, 0, 1)
 
     assert len(visible_agents) == 0
+
+
+def test_observation_dims_no_batch():
+    agent1_coords = torch.Tensor([[0, 0, 0, 0, 1, 1]])
+    agent2_coords = torch.Tensor([[0, 0, 0, 0, 10, 10]])
+    agent3_coords = torch.Tensor([[0, 0, 0, 0, 3, 4]])
+    coords = torch.cat((agent1_coords, agent2_coords, agent3_coords), dim=0)
+
+    agent_grid, plates_grid, doors_grid, goals_grid, coordinates = extract_observation_grids(coords)
+
+    # make sure we have dimensions (batch, agents, obs)
+    assert agent_grid.dim() == 3
+    assert agent_grid.shape == (1, 3, 1)
+    assert plates_grid.dim() == 3
+    assert plates_grid.shape == (1, 3, 1)
+    assert doors_grid.dim() == 3
+    assert doors_grid.shape == (1, 3, 1)
+    assert goals_grid.dim() == 3
+    assert goals_grid.shape == (1, 3, 1)
+    assert coordinates.dim() == 3
+    assert coordinates.shape == (1, 3, 2)
+
+
+def test_observation_dims_batch():
+    agent1_coords = torch.Tensor([[0, 0, 0, 0, 1, 1]])
+    agent2_coords = torch.Tensor([[0, 0, 0, 0, 10, 10]])
+    agent3_coords = torch.Tensor([[0, 0, 0, 0, 3, 4]])
+    coords = torch.stack((agent1_coords, agent2_coords, agent3_coords), dim=1)
+
+    agent_grid, plates_grid, doors_grid, goals_grid, coordinates = extract_observation_grids(coords)
+
+    # make sure we have dimensions (batch, agents, obs)
+    assert agent_grid.dim() == 3
+    assert agent_grid.shape == (1, 3, 1)
+    assert plates_grid.dim() == 3
+    assert plates_grid.shape == (1, 3, 1)
+    assert doors_grid.dim() == 3
+    assert doors_grid.shape == (1, 3, 1)
+    assert goals_grid.dim() == 3
+    assert goals_grid.shape == (1, 3, 1)
+    assert coordinates.dim() == 3
+    assert coordinates.shape == (1, 3, 2)
