de7512859f040918a0e461d5c76a8025b5347723
michele <musacchiomichele@gmail.com>
2024-05-09 17:54:18 +0200

    using main.py to run the training class

--- main.py
+++ main.py
@@ -1,40 +1,13 @@
-import gym
-# need to run the pressure plate import to register the environment with gym
-import pressureplate
-import torch
+from train_agents import TrainingAgents
 
-from encoder import ObservationActionEncoder
-from maddpg import Q
-from observation import extract_observation_grids
-
-if __name__ == "__main__":
-    pressureplate
-    num_players = 4
-
-    # the number of grids that are contained in each observation
-    num_grids = 4
-
-    # the dimensionality of the coordinates, since we are in a 2D world, we only have x,y coordinates
-    dim_coordinates = 2
-
-    env = gym.make(f"pressureplate-linear-{num_players}p-v0")
-
-    max_dist_visibility = env.unwrapped.max_dist
-    # get the shape of the observation space from the environment
-    observation_shape = env.observation_space.spaces[0].shape[0]
-    observation_length = (observation_shape - dim_coordinates) // num_grids
-
-    env.reset()
-
-    # all agents go up
-    action = [0, 0, 0, 0]
-    observations, rewards, dones, _ = env.step(action)
-    observation_stack = torch.Tensor(observations)
-    observation_stack = torch.stack((observation_stack, observation_stack))
-    extract_observation_grids(observation_stack)
-
-    oa_encoder = ObservationActionEncoder(observation_length, max_dist_visibility, 512)
-
-    q_function = Q(agent=0, observation_action_encoder=oa_encoder)
-    q_function(observation_stack, torch.Tensor([action, action]).reshape(2, -1))
-    #oa_encoder(observation_stack, torch.Tensor([[0, 0]]).reshape(2, 1))
+if __name__ == '__main__':
+    training_agents = TrainingAgents(num_agents= 4,
+                                    episodes= 10**1, 
+                                    steps_per_episode= 10**2, 
+                                    batch_size= 32, 
+                                    buffer_size= 10**6, 
+                                    learning_rate= 0.01, 
+                                    gamma= 0.95, 
+                                    tau= 0.1,
+                                    verbose_train= True)
+    training_agents.train()
