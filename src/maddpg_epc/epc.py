bcc796cda2cc7e7ab99d22f1dd64030fdc113a38
jonasbarth <jonas.barth.95@gmail.com>
2024-06-23 11:42:06 +0200

    fill out the crossover in EPC add docstrings

--- src/maddpg_epc/epc.py
+++ src/maddpg_epc/epc.py
@@ -1,30 +1,40 @@
 """Runs the evolutionary population curriculum algorithm using the maddpg algorithm."""
 import abc
+import itertools
+import random
 from typing import Iterable
 
 from maddpg_epc.train_agents import Agent
 
+
 # train K sets of N agents (single role or as many roles as pressure plates?)
 # scale each set from N to cN by copying the set, e.g. c=1.5 by the means of crossover
-    # Crossover
-        # for each set, create all the pairs
-        # do a union of each pair to get 2N agents
-        # sample C pairs
-    # Mutation
-        # for each of the C pairs, run MADDPG fine tuning
-    # Selection
-        # compute fitness score for each agent as average reward over training
-        # pick the top K agents
+# Crossover
+# for each set, create all the pairs
+# do a union of each pair to get 2N agents
+# sample C pairs
+# Mutation
+# for each of the C pairs, run MADDPG fine tuning
+# Selection
+# compute fitness score for each agent as average reward over training
+# pick the top K agents
 
 class EvolutionaryStage(abc.ABC):
-    """Represents an stage in the evolutionary population curriculum algorithm."""
+    """Represents a stage in the evolutionary population curriculum algorithm.
+
+    Each stage operates on k parallel sets of agents and returns k parallel sets of agents.
+    """
 
     def __init__(self, name):
         self.name = name
 
     @abc.abstractmethod
-    def run(self, population: Iterable[Iterable[Agent]]):
-        """Runs the stage on the population."""
+    def run(self, population: Iterable[Iterable[Agent]], **kwargs) -> Iterable[Iterable[Agent]]:
+        """Runs the stage on the population.
+
+        Args:
+            population (Iterable[Iterable[Agent]]): the population of agents.
+        """
         pass
 
 
@@ -37,11 +47,19 @@ class Crossover(EvolutionaryStage):
     such that we now have sets of size 2N. For each role, we then pick a random set from the pairs of agents and combine
     them into another set. Finally, we sample C random pairs.
     """
-    def __init__(self, name = "Crossover"):
+
+    def __init__(self, name="Crossover"):
         super().__init__(name)
 
-    def run(self, population: Iterable[Iterable[Agent]]):
-        pass
+    def run(self, population: Iterable[Iterable[Agent]], **kwargs) -> Iterable[Iterable[Agent]]:
+        """Runs the crossover stage."""
+        agent_pairs = map(set, list(itertools.combinations_with_replacement(population, 2)))
+        agent_pairs = [(pairs[0] | pairs[1]) for pairs in agent_pairs]
+
+        c = kwargs.get('c')
+        sampled_sets = random.sample(agent_pairs, c)
+
+        return sampled_sets
 
 
 class Mutation(EvolutionaryStage):
@@ -52,10 +70,10 @@ class Mutation(EvolutionaryStage):
     dimensional agent parameter space.
     """
 
-    def __init__(self, name = "Mutation"):
+    def __init__(self, name="Mutation"):
         super().__init__(name)
 
-    def run(self, population: Iterable[Iterable[Agent]]):
+    def run(self, population: Iterable[Iterable[Agent]], **kwargs) -> Iterable[Iterable[Agent]]:
         pass
 
 
@@ -66,9 +84,8 @@ class Selection(EvolutionaryStage):
     sets of size 2N.
     """
 
-    def __init__(self, name = "Selection"):
+    def __init__(self, name="Selection"):
         super().__init__(name)
 
-
-    def run(self, population: Iterable[Iterable[Agent]]):
+    def run(self, population: Iterable[Iterable[Agent]], **kwargs) -> Iterable[Iterable[Agent]]:
         pass
